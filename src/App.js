482762eb974f9d3301d1eb8f940dcffd7a3dfbad
Anton <reopik@mail.ru>
2023-08-17 18:47:55 +0300

    made fetching pizzas from mockAPI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import { Route, Routes } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import pizzas from './assets/pizzas.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://64de3b97825d19d9bfb254c6.mockapi.io/items')
+      .then(res => {
+        return res.json();
+      })
+      .then(arr => {
+        setItems(arr)
+      })
+    }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
        <Header />
         <div className="content">
           <Routes>
-            <Route path='/' element={<Home pizzas={pizzas} />} />
+            <Route path='/' element={<Home pizzas={items} />} />
             <Route path='/cart' element={<Cart />} />
           </Routes>
         </div>
