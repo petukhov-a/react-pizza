c9c20f481bd6d7edc2f817f761636c1cb85f92f1
Anton <reopik@mail.ru>
2023-09-05 16:39:06 +0300

    typed filterSlice

--- src/redux/slices/filterSlice.ts
+++ src/redux/slices/filterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
 type Sort = {
@@ -27,21 +27,21 @@ export const filterSlice = createSlice({
   name: 'filter',
   initialState,
   reducers: {
-    setCategoryId(state, action) {
+    setCategoryId(state, action: PayloadAction<number>) {
       state.categoryId = action.payload
     },
-    setSort(state, action) {
+    setSort(state, action: PayloadAction<Sort>) {
         state.sort = action.payload
     },
-    setCurrentPage(state, action) {
+    setCurrentPage(state, action: PayloadAction<number>) {
       state.currentPage = action.payload
     },
-    setFilters(state, action) {
+    setFilters(state, action: PayloadAction<FilterSliceState>) {
       state.currentPage = Number(action.payload.currentPage);
       state.sort = action.payload.sort;
       state.categoryId = Number(action.payload.categoryId);
     },
-    setSearchValue(state, action) {
+    setSearchValue(state, action: PayloadAction<string>) {
       state.searchValue = action.payload;
     }
   },
